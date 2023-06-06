826d0b05ed2b6ee526d3adc1c281d7c26df16852
hardreggaecafe <takamizawa@frembassy.jp>
2023-06-06 23:37:18 +0900

    V-for & Tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,21 @@
 <template>
-    <div class="text-4xl font-bold underline">トップ</div>
+  <div class="flex flex-col"> 
+    <div class="text-4xl font-bold underline">今日のおすすめ</div>
+    <div class="m-3 bg-white rounded-lg shadow">
+      <ul class="divide-y-2 divide-gray-100">
+        <li class="p-3 hover:bg-blue-100" v-for="item in items">
+          {{ item.message }}:　{{ item.price }}円
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
+
+<script>
+ export default {
+  data: () =>{
+  return {
+    items: [{ message: 'たこ焼き', price: 400 }, { message: '串かつセット', price: 1200 }, { message: '男はつらいよ', price: 900 }, { message: '梅くらげ', price: 200 }]
+  }
+}}
+</script>
