833589902fd45314aa6042126a310dce52f87a34
hardreggaecafe <takamizawa@frembassy.jp>
2023-06-07 16:47:15 +0900

    @apply

--- pages/index.vue
+++ pages/index.vue
@@ -10,10 +10,10 @@
     </div>
     <div id="app">
       <div class="col-5">
-        <input v-model="name" type="text" name="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-        <div class="block mb-2 text-sm font-medium text-red-700 dark:text-red-500">{{ name }}</div>
-        <button @click="show()" class="m-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">show</button> 
-        <button @click="change()" class="m-1 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">change</button>
+        <input v-model="name" type="text" name="name" class="textbox" />
+        <div class="message">{{ name }}</div>
+        <button @click="show()" class="button-blue">show</button> 
+        <button @click="change()" class="button-red">change</button>
       </div>
     </div>
   </div>
