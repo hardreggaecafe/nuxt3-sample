98595916e1ede504f236c85d1e30ce750116b8be
hardreggaecafe <takamizawa@frembassy.jp>
2023-06-07 17:57:33 +0900

    Validation

--- pages/index.vue
+++ pages/index.vue
@@ -10,10 +10,12 @@
     </div>
     <div id="app">
       <div class="col-5">
+        <label for="name">注文の商品名</label>
         <input v-model="name" type="text" name="name" class="textbox" />
-        <div class="message">{{ name }}</div>
-        <button @click="show()" class="button-blue">show</button> 
-        <button @click="change()" class="button-red">change</button>
+        <div class="message" v-if="isInvalidName">{{ name }}は商品名として短すぎます</div>
+        <div class="message" v-if="isBlank">何か入力してください</div>
+        <button @click="order()" class="button-blue">注文</button> 
+        <button @click="change()" class="button-red">変更</button>
       </div>
     </div>
   </div>
@@ -24,14 +26,21 @@
   data: () =>{
   return {
     items: [{ message: 'たこ焼き', price: 400 }, { message: '串かつセット', price: 1200 }, { message: '男はつらいよ', price: 900 }, { message: '梅くらげ', price: 200 }],
-    name: 'this is name'
+    name: 'いつものやつ'
   }
   }, methods: {
-    show: function () {
-      alert(this.name);
+    order: function () {
+      alert(this.name+"をご注文");
     },
     change: function () {
-      this.name = "test!!!";
+      this.name = "いつものやつ";
+    }
+  }, computed: {
+    isInvalidName(){
+      return this.name.length < 4 && this.name.length > 0;
+    },
+    isBlank(){
+      return this.name.length == 0;
     }
   }
 }
