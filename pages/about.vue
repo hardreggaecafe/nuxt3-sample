20c6f2d1794d8e58d0e6c2481adae16a24c126d8
hardreggaecafe <takamizawa@frembassy.jp>
2023-06-08 09:46:10 +0900

    V-on

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,21 @@
 <template>
-    <div class="text-6xl font-bold underline">ABOUT</div>
+    <div class="flex flex-col"> 
+        <div class="text-6xl font-bold underline">ABOUT</div>
+
+        <section>
+            <img src = "../assets/img/oit_logo.png" alt="OIT logo" width="200" v-on:mousemove="onImgMouseMove" >
+            <p>ポインターの位置： x={{ mousePointerX }}; y={{ mousePointerY }}</p>
+        </section>
+    </div>
 </template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+
+const mousePointerX = ref(0);
+const mousePointerY = ref(0);
+const onImgMouseMove = (event: MouseEvent): void => {
+    mousePointerX.value = event.offsetX;
+    mousePointerY.value = event.offsetY;
+};
+</script>
