d8d5aa99024a19923da2c18bf79de3c3639d5912
hardreggaecafe <takamizawa@frembassy.jp>
2023-06-11 10:47:34 +0900

    Review detail

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="bg-gray-500 p-3 text-white flex flex-col items-center">
+  <div class="bg-gray-500 w-1/7 p-3 text-white flex flex-col items-center">
     <div><nuxt-link to="/">トップ</nuxt-link></div>
     <div><nuxt-link to="/about">アバウト</nuxt-link></div>
-    <div><nuxt-link to="/list">レビュー</nuxt-link></div>
+    <div><nuxt-link to="/lists">レビュー</nuxt-link></div>
   </div>
 </template>
---
+++ pages/lists/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex flex-col">
+        <div>
+            <h1 class="m-3 text-4xl font-bold">当店のレビュー一覧</h1>
+        </div>
+        <ReviewList :reviews="reviews.contents" />
+    </div>
+</template>
+
+<script setup lang="ts">
+    const {data:reviews} = await useFetch('/api/reviewList')
+</script>
